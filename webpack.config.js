78055643beff563d6d65ea68752641edf1f6592c
wbxzjh1314 <1648798671@qq.com>
2017-04-03 22:32:55 +0800

    文件通用引用添加

--- webpack.config.js
+++ webpack.config.js
@@ -34,5 +34,17 @@ module.exports = {
             // 处理.vue文件
             {test: /\.vue$/, loader: "vue-loader"}
         ]
+    },
+    //其它解决方案配置（这里是省略后缀名的的书写地方）
+    resolve: {
+        ////查找module的话从这里开始查找
+        //root: __dirname + "/assets/js", //绝对路径
+        ////自动扩展文件后缀名，意味着我们require模块可以省略不写后缀名
+        //extensions: ['', '.js', '.json', '.scss'],
+        //模块别名定义，方便后续直接引用别名，无须多写长长的地址
+        alias: {
+            // 一些工具类第三方库文件等
+            //js: path.resolve(__dirname + "/assets/js")
+        }
     }
 };
